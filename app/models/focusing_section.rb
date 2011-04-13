f6a8c55f555cadfca66ec62e0cb172c12e12349b
Aleksi <alex2209@luukku.com>
2011-04-13 09:48:35 +0300

    posting added and user can see posts

---
+++ app/models/focusing_section.rb
@@ -0,0 +1,5 @@
+class FocusingSection < ActiveRecord::Base
+ belongs_to :industry
+  validates_presence_of :name, :industry_id
+
+end
---
+++ app/models/industry.rb
@@ -0,0 +1,5 @@
+class Industry < ActiveRecord::Base
+ has_many :focusing_section
+   validates_presence_of :name
+
+end
